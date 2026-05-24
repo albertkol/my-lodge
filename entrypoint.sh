afcf4c56521325e1abe441a19c6a393ac6ed8c05
Albert Artur Kolozsvari <albertartk@gmail.com>
2026-05-24 20:33:16 +0100

    Chore: Adjust gunicorn settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 git clone "https://x-access-token:${LODGE_BOOKS_TOKEN}@github.com/albertkol/lodge-books.git" /lodge-books
 
-exec /app/.venv/bin/gunicorn my_lodge.app:app --bind "0.0.0.0:${PORT:-8000}" --workers 2
+exec /app/.venv/bin/gunicorn my_lodge.app:app --bind "0.0.0.0:${PORT:-8000}" --workers 2 --timeout 300
